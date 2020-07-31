074857e27b3eb726bfcb96d13e57ebd6b8b847f5
Dani Coleman <danicoleman00@gmail.com>
2020-07-30 19:50:46 -0600

    Create file_reader test class

---
+++ test/file_reader_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/file_reader'
+
+class FileReaderTest < Minitest::Test
+
+  def test_it_exists
+    file_reader = FileReader.new
+    assert_instance_of FileReader, file_reader
+  end
+end
