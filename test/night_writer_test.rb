8656269af730c156a850e16d93864e8ae512f7f8
Dani Coleman <danicoleman00@gmail.com>
2020-07-31 19:18:40 -0600

    Add exitst test

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,21 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/night_writer'
-require './lib/file_reader'
+# require './lib/reader_file'
 
 class NightWriterTest < Minitest::Test
 
   def test_is_exists
-    night_writer = NightWriter.new
+    message = "message.txt"
+    braille = "braille.txt"
+    ARGV[0] = message
+    ARGV[1] = braille
+    night_writer = NightWriter.new(message, braille)
+
     assert_instance_of NightWriter, night_writer
   end
 
-  def test_can_encode_file_to_braille
-    skip
-  end
-
-  def test_can_encode_to_braille
-    skip
-  end
 
 end
