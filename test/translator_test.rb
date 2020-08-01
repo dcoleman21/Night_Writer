c579c9d804d9bf86ff5325699918992a1e15ec2b
Dani Coleman <danicoleman00@gmail.com>
2020-07-31 19:33:54 -0600

    Add exist method test

---
+++ test/translator_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator'
+
+class TranslatorTest < Minitest::Test
+
+  def test_it_exists
+    translator = Translator.new
+    assert_instance_of Translator, translator
+  end
+
+end
