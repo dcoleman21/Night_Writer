8732a5e66e8832c8e4166be35d0b3f62f17f1c89
Dani Coleman <danicoleman00@gmail.com>
2020-08-01 12:10:49 -0600

    Fix typing error in new file instantiation

--- test/reader_file_test.rb
+++ test/reader_file_test.rb
@@ -1,18 +1,17 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/night_writer'
-# require './lib/reader_file'
+require './lib/reader_file'
 
-class NightWriterTest < Minitest::Test
+class ReaderFileTest < Minitest::Test
 
   def test_is_exists
     message = "message.txt"
     braille = "braille.txt"
     ARGV[0] = message
     ARGV[1] = braille
-    night_writer = NightWriter.new(message, braille)
+    reader_file = ReaderFile.new
 
-    assert_instance_of NightWriter, night_writer
+    assert_instance_of ReaderFile, reader_file
   end
 
 
