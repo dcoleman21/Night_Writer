bcb2951b039a357a267847f54333c7e748737172
Dani Coleman <danicoleman00@gmail.com>
2020-08-01 12:07:42 -0600

    Add method that encodes message to braille

--- lib/night_writer_class.rb
+++ lib/night_writer_class.rb
@@ -1,22 +1,26 @@
 require_relative 'reader_file'
+require_relative 'translator'
 
 class NightWriter
   attr_reader :message
-              # :braille
+  #             :braille
 
   def initialize(message, braille)
     @reader = ReaderFile.new
     @message = @reader.read
+    @translator = Translator.new
     @braille = braille
     command_line_output
+    encode_message_to_braille
   end
 
-  # def encode_to_braille(message)
-  #   #add encode method from translator class
-  # end
+  def encode_message_to_braille
+    message = @message.gsub(" ", "")
+    encoded = @translator.encode_to_braille(message)
+    File.write(@braille, encoded)
+  end
 
   def command_line_output
-    puts "Created '#{@braille}' containing #{@message.chomp.length} characters"
-    File.write(@braille, File.read(ARGV[0]))
+    puts "Created '#{@braille}' containing #{@message.length} characters"
   end
 end
