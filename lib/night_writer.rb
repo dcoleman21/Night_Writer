4bf0da7773b2bd39384b728a4efdc305b18a52b1
Dani Coleman <danicoleman00@gmail.com>
2020-07-31 19:18:06 -0600

    Create runner file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,29 +1,5 @@
-class FileReader
-  def read
-    filename = ARGV[0]
-    File.read(filename)
-  end
-end
+require './lib/night_writer_class'
 
-class NightWriter
-  attr_reader :file_reader
+message, braille = ARGV
 
-  def initialize
-    @reader = FileReader.new
-  end
-
-  def encode_file_to_braille
-    # I wouldn't worry about testing this method
-    # unless you get everything else done
-    plain = reader.read
-    braille = encode_to_braille(plain)
-  end
-
-  def encode_to_braille(input)
-    # you've taken in an INPUT string
-    # do the magic
-    # send out an OUTPUT string
-  end
-end
-
-puts ARGV.inspect
+NightWriter.new(message, braille)
