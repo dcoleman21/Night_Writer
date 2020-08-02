7984b32404532b1a552e6caa5e0508e923959726
Dani Coleman <danicoleman00@gmail.com>
2020-08-01 22:42:20 -0600

    WIP to get read and write files functioning

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,30 @@
-require_relative 'night_writer_class'
+require_relative 'alpha_to_braille'
 
-message, braille = ARGV
+class NightWriter
+  attr_reader :reader
 
-NightWriter.new(message, braille)
+  def initialize
+    @reader = File.new("message.txt")
+  end
+
+  def encode_file_to_braille
+    plain = reader.read
+    braille = to_braille(string)
+
+    write_encoded_braille_to_text_file(braille)
+  end
+
+  def encode_to_braille(input)
+    braille_1 = AlphaToBraille.new
+    braille_1.to_braille(input)
+    # braille_1.braille_translation
+  end
+
+  def write_encoded_braille_to_text_file(braille)
+    File.write("braille.txt", braille)
+  end
+  # english = BRAILLE_HASH.invert
+
+end
+new_night_writer = NightWriter.new
+new_night_writer.encode_file_to_braille
