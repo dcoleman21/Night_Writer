e216855f979a4e0fc47488b0b00263f844478795
Dani Coleman <danicoleman00@gmail.com>
2020-08-02 16:15:08 -0600

    Add requires for all files added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,9 @@ require 'simplecov'
 SimpleCov.start
 
 require 'minitest/autorun'
-require 'minitest/pride' 
+require 'minitest/pride'
+require './test/test_helper'
+require './lib/alpha_to_braille'
+require './lib/braille_to_alpha'
+require './lib/night_writer'
+require './lib/translateable'
