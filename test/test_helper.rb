43420d0329a25afc8ac5b564e37283e6e05d9722
Dani Coleman <danicoleman00@gmail.com>
2020-08-02 18:28:22 -0600

    Update file with all necessary requires

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,5 @@ SimpleCov.start
 
 require 'minitest/autorun'
 require 'minitest/pride'
-require './test/test_helper'
 require './lib/alpha_to_braille'
 require './lib/braille_to_alpha'
-require './lib/night_writer'
-require './lib/translateable'
